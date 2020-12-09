1aa0e222d1f16c52f8eba78f95d4e1fc12564245
Riqza <riqzas@ymail.com>
2020-12-09 20:01:16 +0500

    commit state fix

--- src/favrt.js
+++ src/favrt.js
@@ -1,29 +1,34 @@
 import React, { Component } from 'react';
 import './App.css';
 
-class Fvrt extends Component{
-  constructor(props){
+class Fvrt extends Component {
+  constructor(props) {
     super(props);
-    this.state={
-      breed:'',
-      newData:[],
-      favData:[]
+    this.state = {
+      breed: '',
+      newData: [],
+      favData: []
     }
   }
-  
- render(){
-    const favData = this.state.favData;
-    console.log(this.props,"abc112", this.props.handleDelete)
-     return(
-         <div className="maindiv">
-        <div className='favrtImg'> 
-      { this.props.favrtImages.map((data,i) =>
-        <img className="image" src={data.dogs} onClick={(e) => this.props.handleDelete(i) }/>
-        )}
-      </div>
 
-         </div>
-     );
- }
+  render() {
+    const favData = this.props.favrtImages;
+    console.log("fav", favData)
+    console.log(this.props, "abc112", this.props.handleDelete)
+    return (
+      <div className="maindiv">
+        <div className='favrtImg'>
+          {this.props.favrtImages.map((data, i) =>
+            <img className="image" src={data.dogs} onClick={
+              (e) => {
+                debugger
+                this.props.handleDelete(i)
+              }} />
+          )}
+        </div>
+
+      </div>
+    );
+  }
 }
- export default Fvrt;
+export default Fvrt;
--- src/main.js
+++ src/main.js
@@ -2,72 +2,73 @@ import React, { Component } from 'react';
 import Fvrt from './favrt.js'
 import './App.css';
 
-class FetchImages extends Component{
-  constructor(props){
+class FetchImages extends Component {
+  constructor(props) {
     super(props);
-    this.state={
-      breed:'',
-      newData:[],
-      favData:[]
+    this.state = {
+      breed: '',
+      newData: [],
+      favData: []
     }
   }
-  
+
   async showImages(breed) {
-   const response = await fetch(`https://dog.ceo/api/breed/${breed}/images/random/10`)
+    const response = await fetch(`https://dog.ceo/api/breed/${breed}/images/random/10`)
     const data = await response.json();
-      const items = data.message.map((item, index) =>{
-        return{id:index, dogs:item, marked:false}
-      })
-      this.setState({ newData:items})
-       }
-   handleFavourite (e,data){
-  let  arr= this.state.newData.map((item, index) =>{ 
-    if (item.dogs === data.dogs) {
-      if(item.marked == false){
-        item.marked = true
-        this.setState({ favData:[...this.state.favData,item]}) 
+    const items = data.message.map((item, index) => {
+      return { id: index, dogs: item, marked: false }
+    })
+    this.setState({ ...this.state, newData: items })
+  }
+  handleFavourite(e, data) {
+    let arr = this.state.newData.map((item, index) => {
+      if (item.dogs === data.dogs) {
+        if (item.marked == false) {
+          item.marked = true
+          this.setState({ favData: [...this.state.favData, item] })
+
+        }
+        return item
       }
       return item
-    }
-    return item
     })
-    this.setState({newData:arr})
+    this.setState({ newData: arr })
     //console.log("arr",arr);
+  }
+
+  render() {
+    let newData = this.state.newData;
+    const handleDelete = (i) => {
+      let favData = this.state.favData;
+      favData.splice(i, 1);
+      this.setState({
+        ...this.state,
+        favData: favData
+      });
     }
-    handleDelete(i){
-        let favData = this.state.favData;
-        favData.splice(i,1);
-        this.setState({
-            favData: favData
-          });
-    }
-    
-  render(){
-   let newData = this.state.newData;
-   
-  return(
-    <div>
-      <input    className="txt"
-                type="text"
-                placeholder="Enter breed"
-                value={this.state.breed}
-                onChange={e => this.setState({ breed: e.target.value })}
-              />
-      <button className="btn" onClick={e => this.showImages(this.state.breed)}>Submit</button>
+    return (
+      <div>
+        <input className="txt"
+          type="text"
+          placeholder="Enter breed"
+          value={this.state.breed}
+          onChange={e => this.setState({ ...this.state, breed: e.target.value })}
+        />
+        <button className="btn" onClick={e => this.showImages(this.state.breed)}>Submit</button>
 
         <div className="fetchImg">
-      {newData.map((data,i) => 
-        <img className="image" src={data.dogs} onClick={(e) => this.handleFavourite(e,data) }/>
-        )}
-       </div> 
+          {newData.map((data, i) =>
+            <img className="image" src={data.dogs} onClick={(e) => this.handleFavourite(e, data)} />
+          )}
+        </div>
 
-      <h3 className="favtText"> Favourite Images </h3>
-      <div>
-        <Fvrt favrtImages = {this.state.favData} />
+        <h3 className="favtText"> Favourite Images </h3>
+        <div>
+          <Fvrt favrtImages={this.state.favData} handleDelete={handleDelete} />
+        </div>
       </div>
-    </div>
-  );
-      } 
+    );
+  }
 }
 
 export default FetchImages;
